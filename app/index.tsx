61f9860632734a13f6aa1e71e9a547caa0f1dde1
DanielTruscaV1 <danieltrusca2008@gmail.com>
2024-08-23 01:28:36 +0300

    Small update.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
 import { Text, View } from "react-native";
+import { Link } from 'expo-router';
 
 export default function Index() {
+
+  const welcome_message = "Welcome to Daniel's app.";
+
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>{welcome_message}</Text>
+      <Link href={"/profile" as any}>
+        <Text style={{ color: 'blue' }}>View Profile</Text>
+      </Link>
     </View>
   );
 }
